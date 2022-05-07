caa7d10cb21cb6b16b7b097fd9ac710b4338d23f
Bartek Dziadosz <bdziadosz96@icloud.com>
2022-05-07 17:06:12 +0200

    Init kafka application

---
+++ src/main/java/com/store/application/kafkastore/KafkaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.store.application.kafkastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/store/application/kafkastore/KafkaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.application.kafkastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
